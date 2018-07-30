affa427da609270de83b78b281d88b1d00edc80f
JosFloy <445843692@qq.com>
2018-07-30 19:25:50 +0800

    完成启动界面，并利用timer定时向主界面跳转

--- app/src/main/java/com/josfloy/writenumber/StartActivity.java
+++ app/src/main/java/com/josfloy/writenumber/StartActivity.java
@@ -1,13 +1,34 @@
 package com.josfloy.writenumber;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
-public class StartActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class StartActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //代码中实现全屏
+        //        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+      /*  this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);*/
         setContentView(R.layout.activity_start);
+
+        Timer timer = new Timer();
+        //创建TimerTask
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                //从启动界面跳转到游戏主界面
+                startActivity(new Intent(StartActivity.this, MainActivity.class));
+                finish();
+            }
+        };
+
+        timer.schedule(timerTask, 2000);//设置显示启动界面2s后，跳转到游戏主界面
     }
 }
